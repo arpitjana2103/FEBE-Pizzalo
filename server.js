c21a96a509c0494e1e03800933f7edcc153e2c68
Arpit Jana <arpitjana2103@gmail.com>
2023-07-10 06:50:59 +0530

    Express Server Created

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', function (res, req) {
+    res.render('home');
+});
+
+// Set Template
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+app.listen(PORT, function () {
+    console.log(`Server : http://localhost:${PORT}`);
+});
